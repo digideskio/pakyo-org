280cca81b7fa1ae4b467893592ecafc03a3c8eee
Bryan Powell <bryan@metabahn.com>
2015-04-10 20:49:44 -0500

    Fix improper code formatting

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -8,10 +8,10 @@ class Formatter
 
       begin
         lexer_name = code.text.split("\n")[0].gsub(':', '')
-        code.inner_html = Pygments.highlight(code.inner_html.split("\n")[1..-1].join("\n").strip.gsub(/&lt;|&gt;/) {|s| {'&lt;' => '<','&gt;' => '>'}[s]}, :lexer => lexer_name)
-        code.set_attribute('class', lexer_name)
-        pre.replace(code)
-      rescue
+        content = Nokogiri::HTML.fragment(Pygments.highlight(code.inner_html.split("\n")[1..-1].join("\n").strip.gsub(/&lt;|&gt;/) {|s| {'&lt;' => '<','&gt;' => '>'}[s]}, :lexer => lexer_name))
+        pre.replace(content)
+      rescue e
+        puts e.message
       end
 
     end
